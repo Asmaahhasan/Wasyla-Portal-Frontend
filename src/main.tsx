4d70f53c30c60856755a12710e07d866d9f32b16
Asmaa <Asmaa@QUEEN>
2026-08-02 00:04:09 +0300

    Add automatic HTTPS redirect in main.tsx and rebuild dist

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,16 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
+// Enforce HTTPS on production domain
+if (
+  typeof window !== 'undefined' &&
+  window.location.protocol === 'http:' &&
+  window.location.hostname !== 'localhost' &&
+  window.location.hostname !== '127.0.0.1'
+) {
+  window.location.replace(window.location.href.replace(/^http:/i, 'https:'));
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
